e2d44d295f183333a46e5c4b679275ed145d7161
reyjach <reymuerto14@hotmail.com>
2020-01-27 03:08:09 -0500

    se creo el proyecto y se modifico el index.html y se creo el componente de Heade

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React ,{Component} from 'react';
+import Noticias from './Componentes/Header'
+
+class App extends Component{
+
+  render() {
+    return (
+      <div className="">
+        <Noticias titulo = 'Noticias'></Noticias>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
